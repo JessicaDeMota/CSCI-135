4844d0b56fee5e5d461298520e4680b220e88b88
JessicaDeMota <32750464+JessicaDeMota@users.noreply.github.com>
2021-01-21 00:42:48 -0500

    Add files via upload

---
+++ CSCI 135/E1.7.cpp
@@ -0,0 +1,26 @@
+/*
+Author: Jessica De Mota Munoz 
+Course: CSCI-135 
+Instructor: Mr.Maryash 
+Assignment: Names,E1.7 
+
+This program prints names on different lines
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name1;
+    string name2;
+    string name3;
+
+    cout << "Enter a name:" ;
+    cin >> name1 >> name2 >> name3; 
+    cout << name1 << endl;
+    cout << name2 << endl; 
+    cout << name3 << endl; 
+    
+    return 0;
+}
